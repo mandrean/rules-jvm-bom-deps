4f8d8c5c5d85b764f0392274be2de0d43f7ace54
Sebastian Mandrean <sebastian.mandrean@gmail.com>
2019-09-26 14:23:47 +0200

    Init repo

---
+++ src/main/java/se/mandrean/rulesjvmbomdeps/RulesJvmBomDeps.java
@@ -0,0 +1,9 @@
+package src.main.java.se.mandrean.rulesjvmbomdeps;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class RulesJvmBomDeps {
+
+  @JsonProperty("some_field")
+  private String someField;
+}
